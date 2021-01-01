d21d5f10e338ddbc19b3f51e9bfb64407bf16d8c
Vladislav Sumin <vladislavsumin@mail.ru>
2021-01-01 20:16:11 +0300

    [server] fix cams share

--- server/src/main/kotlin/ru/falseteam/control/server/domain/cams/CamsInteractorImpl.kt
+++ server/src/main/kotlin/ru/falseteam/control/server/domain/cams/CamsInteractorImpl.kt
@@ -5,26 +5,25 @@ import com.squareup.sqldelight.runtime.coroutines.mapToList
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.flow.*
-import kotlinx.coroutines.withContext
 import ru.falseteam.control.api.dto.CameraDTO
 import ru.falseteam.control.server.database.Camera
 import ru.falseteam.control.server.database.CameraQueries
 
 class CamsInteractorImpl(
-    private val cameraQueries: CameraQueries
+    cameraQueries: CameraQueries
 ) : CamsInteractor {
-    override fun observeAll(): Flow<List<CameraDTO>> =
-        cameraQueries.selectAll()
-            .asFlow()
-            .mapToList()
-            .flowOn(Dispatchers.IO)
-            .map { list ->
-                list.map { it.toDTO() }
-            }.shareIn(
-                scope = GlobalScope,
-                started = SharingStarted.WhileSubscribed(replayExpirationMillis = 0),
-                replay = 1
-            )
+    private val allObservable = cameraQueries.selectAll()
+        .asFlow()
+        .mapToList()
+        .flowOn(Dispatchers.IO)
+        .map { list -> list.map { it.toDTO() } }
+        .shareIn(
+            scope = GlobalScope,
+            started = SharingStarted.WhileSubscribed(replayExpirationMillis = 0),
+            replay = 1
+        )
+
+    override fun observeAll(): Flow<List<CameraDTO>> = allObservable
 
     private fun Camera.toDTO(): CameraDTO {
         return CameraDTO(
