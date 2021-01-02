67d40f313af6f9b7bc3bc83344cf560243b89075
Vladislav Sumin <vladislavsumin@mail.ru>
2021-01-02 18:54:23 +0300

    [server] add cams processing template

--- server/src/main/kotlin/ru/falseteam/control/server/domain/cams/CamsConnectionInteractorImpl.kt
+++ server/src/main/kotlin/ru/falseteam/control/server/domain/cams/CamsConnectionInteractorImpl.kt
@@ -1,13 +1,41 @@
 package ru.falseteam.control.server.domain.cams
 
-import kotlinx.coroutines.delay
+import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
+import org.slf4j.LoggerFactory
+import ru.falseteam.control.api.dto.CameraDTO
 
 class CamsConnectionInteractorImpl(
     private val camsInteractor: CamsInteractor
 ) : CamsConnectionInteractor {
-    override suspend fun processConnections() {
-        while (true) {
-            delay(10000)
+    private val log = LoggerFactory.getLogger("controls.cams")
+
+    override suspend fun processConnections() = coroutineScope {
+        log.debug("Start process camera connection")
+        try {
+            camsInteractor.observeAll().collectLatest {
+                processCams(it)
+            }
+        } finally {
+            log.debug("Stopped process camera connection")
+        }
+    }
+
+    private suspend fun processCams(cams: List<CameraDTO>) = coroutineScope {
+        try {
+            log.debug("Start process cams: ${cams.joinToString { it.name }}")
+            cams.forEach { launch { processCamera(it) } }
+        } finally {
+            log.debug("Stopped process cams")
+        }
+    }
+
+    private suspend fun processCamera(camera: CameraDTO) {
+        try {
+            log.trace("Start process camera ${camera.name}")
+        } finally {
+            log.trace("Stopped process camera ${camera.name}")
         }
     }
 }
