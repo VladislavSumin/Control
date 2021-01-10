9a33fc58b52297fa029c241bc6c69e91c530edcb
Vladislav Sumin <vladislavsumin@mail.ru>
2021-01-10 17:34:20 +0300

    [all] update android plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ buildscript {
         jcenter()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:7.0.0-alpha03")
+        classpath("com.android.tools.build:gradle:7.0.0-alpha04")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.21")
         classpath("com.squareup.sqldelight:gradle-plugin:1.4.3")
     }
