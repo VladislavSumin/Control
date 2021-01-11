72e5d73ae2674b6cf7f12df2e8008135ec21e929
Vladislav Sumin <vladislavsumin@mail.ru>
2021-01-11 22:22:26 +0300

    [client] add flow policy

--- api/src/main/kotlin/ru/falseteam/control/api/rsub/CamsRSub.kt
+++ api/src/main/kotlin/ru/falseteam/control/api/rsub/CamsRSub.kt
@@ -2,9 +2,11 @@ package ru.falseteam.control.api.rsub
 
 import kotlinx.coroutines.flow.Flow
 import ru.falseteam.control.api.dto.CameraDTO
+import ru.falseteam.rsub.RSubFlowPolicy
 import ru.falseteam.rsub.RSubInterface
 
 @RSubInterface("cams")
 interface CamsRSub {
+    @RSubFlowPolicy(RSubFlowPolicy.Policy.SUPPRESS_EXCEPTION_AND_RECONNECT)
     fun observeAll(): Flow<List<CameraDTO>>
 }
--- api/src/main/kotlin/ru/falseteam/control/api/rsub/CamsStatusRSub.kt
+++ api/src/main/kotlin/ru/falseteam/control/api/rsub/CamsStatusRSub.kt
@@ -3,9 +3,11 @@ package ru.falseteam.control.api.rsub
 import kotlinx.coroutines.flow.Flow
 import ru.falseteam.control.api.dto.CameraDTO
 import ru.falseteam.control.api.dto.CameraStatusDTO
+import ru.falseteam.rsub.RSubFlowPolicy
 import ru.falseteam.rsub.RSubInterface
 
 @RSubInterface("cams_status")
 interface CamsStatusRSub {
+    @RSubFlowPolicy(RSubFlowPolicy.Policy.SUPPRESS_EXCEPTION_AND_RECONNECT)
     fun observeAll(): Flow<Map<Long, CameraStatusDTO>>
 }
