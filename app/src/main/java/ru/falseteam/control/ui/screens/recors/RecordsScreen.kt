4d10b8b72057dbc67b8dddb5919d24a2bbb76dbe
Vladislav Sumin <vladislavsumin@mail.ru>
2021-01-14 23:09:14 +0300

    [client] add player caching

--- app/src/main/java/ru/falseteam/control/ui/screens/recors/RecordsScreen.kt
+++ app/src/main/java/ru/falseteam/control/ui/screens/recors/RecordsScreen.kt
@@ -1,8 +1,10 @@
 package ru.falseteam.control.ui.screens.recors
 
+import android.content.Context
 import android.net.Uri
-import androidx.compose.foundation.ScrollableColumn
+import android.util.AttributeSet
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.Card
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Surface
@@ -16,6 +18,7 @@ import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.platform.AmbientContext
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.util.Pools
 import androidx.navigation.NavController
 import com.google.android.exoplayer2.MediaItem
 import com.google.android.exoplayer2.SimpleExoPlayer
@@ -46,13 +49,20 @@ fun RecordsScreen(navController: NavController, viewModel: RecordsViewModel = ko
 
 @Composable
 private fun RecordsList(records: List<CameraRecordDTO>) {
-    ScrollableColumn {
-        records.forEach { RecordCard(record = it) }
+    val context = AmbientContext.current
+    val playerCache = remember {
+        PlayerCache(context)
+    }
+
+    LazyColumn {
+        items(records) { record ->
+            RecordCard(record = record, playerCache)
+        }
     }
 }
 
 @Composable
-private fun RecordCard(record: CameraRecordDTO) {
+private fun RecordCard(record: CameraRecordDTO, playerCache: PlayerCache) {
     Card(
         shape = RectangleShape,
         elevation = 2.dp,
@@ -61,7 +71,7 @@ private fun RecordCard(record: CameraRecordDTO) {
             .padding(0.dp, 4.dp)
     ) {
         Column(modifier = Modifier.fillMaxWidth()) {
-            VideoRecord(record.id)
+            VideoRecord(record.id, playerCache)
             Text(text = record.timestamp.toString())
             Text(text = record.fileSize.toString())
             Text(text = record.length.toString())
@@ -70,38 +80,59 @@ private fun RecordCard(record: CameraRecordDTO) {
 }
 
 @Composable
-private fun VideoRecord(id: Long) {
+private fun VideoRecord(id: Long, playerCache: PlayerCache) {
     Surface {
-        val context = AmbientContext.current
-
-        val factory = remember {
-            val dataSourceFactory: DataSource.Factory = DefaultDataSourceFactory(
-                context,
-                Util.getUserAgent(context, context.packageName)
-            )
-
-            ProgressiveMediaSource.Factory(dataSourceFactory)
-        }
-
-        val exoPlayer = remember {
-            SimpleExoPlayer.Builder(context).build()
-        }
-
-        val exoPlayerView = remember {
-            PlayerView(context).apply {
-                player = exoPlayer
-            }
-        }
-
         AndroidView(
             modifier = Modifier
                 .fillMaxWidth()
                 .aspectRatio(16 / 9f),
-            viewBlock = { exoPlayerView }) {
-            val uri = Uri.parse("http://10.0.0.56:8080/api/v1/record_video/$id")
-            val mediaItem = MediaItem.Builder().setUri(uri).build()
-            exoPlayer.setMediaSource(factory.createMediaSource(mediaItem))
-            exoPlayer.prepare()
+            viewBlock = {
+                playerCache.acquire()
+            }) {
+            it.player?.apply {
+                this as SimpleExoPlayer
+                setMediaSource(playerCache.createMedia(id))
+                prepare()
+            }
         }
+    }
+}
+
+private class PlayerCache(
+    private val context: Context
+) {
+    private val pool = Pools.SimplePool<MyPlayer>(10)
+    private val mediaFactory by lazy {
+        val dataSourceFactory: DataSource.Factory = DefaultDataSourceFactory(
+            context,
+            Util.getUserAgent(context, context.packageName)
+        )
+
+        ProgressiveMediaSource.Factory(dataSourceFactory)
+    }
+
+    fun createMedia(id: Long): ProgressiveMediaSource {
+        val uri = Uri.parse("http://10.0.0.56:8080/api/v1/record_video/$id")
+        val mediaItem = MediaItem.Builder().setUri(uri).build()
+        return mediaFactory.createMediaSource(mediaItem)
+    }
+
+    fun acquire(): MyPlayer = pool.acquire() ?: MyPlayer(context).apply {
+        player = SimpleExoPlayer.Builder(context).build()
+    }
+
+    fun release(player: MyPlayer) = pool.release(player)
+}
+
+private class MyPlayer @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null, defStyleAttr: Int = 0
+) : PlayerView(context, attrs, defStyleAttr) {
+    var cache: PlayerCache? = null
+
+    override fun onDetachedFromWindow() {
+        super.onDetachedFromWindow()
+        player?.stop()
+        cache?.release(this)
     }
 }
