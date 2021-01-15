033ff8ec798471b2772d89f4387f7c4a45979e4d
Vladislav Sumin <vladislavsumin@mail.ru>
2021-01-15 20:00:05 +0300

    [client] add settings screen && refactoring ui package

---
+++ app/src/main/java/ru/falseteam/control/ui/screens/settings/SettingsScreen.kt
@@ -0,0 +1,9 @@
+package ru.falseteam.control.ui.screens.settings
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+
+@Composable
+fun SettingsScreen(navController: NavController) {
+
+}
