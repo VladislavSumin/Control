3e476b414835301c50ce0a113249d274e8acc8ae
Vladislav Sumin <vladislavsumin@mail.ru>
2021-01-17 21:12:55 +0300

    [client] update compose version from 1.0.0-alpha09 to 1.0.0-alpha10

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,7 +40,7 @@ android {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.0.0-alpha09"
+        kotlinCompilerExtensionVersion = "1.0.0-alpha10"
         kotlinCompilerVersion = "1.4.21"
     }
 }
@@ -60,10 +60,10 @@ dependencies {
     implementation("androidx.core:core-ktx:1.3.2")
     implementation("androidx.appcompat:appcompat:1.2.0")
     implementation("com.google.android.material:material:1.2.1")
-    implementation("androidx.compose.ui:ui:1.0.0-alpha09")
-    implementation("androidx.compose.material:material:1.0.0-alpha09")
-    implementation("androidx.compose.ui:ui-tooling:1.0.0-alpha09")
-    implementation("androidx.navigation:navigation-compose:1.0.0-alpha04")
+    implementation("androidx.compose.ui:ui:1.0.0-alpha10")
+    implementation("androidx.compose.material:material:1.0.0-alpha10")
+    implementation("androidx.compose.ui:ui-tooling:1.0.0-alpha10")
+    implementation("androidx.navigation:navigation-compose:1.0.0-alpha05")
     implementation("com.russhwolf:multiplatform-settings:0.7")
     implementation("com.google.android.exoplayer:exoplayer:2.12.2")
     testImplementation("junit:junit:4.13.1")
