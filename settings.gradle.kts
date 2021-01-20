82d7fac031437f1333dfa54eda3fafb247b23d95
Vladislav Sumin <vladislavsumin@mail.ru>
2021-01-20 22:19:07 +0300

    [global] enable build cache && enable gradle build scan

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,18 @@
+plugins {
+    id("com.gradle.enterprise") version "3.5.1"
+}
+
 rootProject.name = "Control"
-
 includeBuild("../rSub")
-
 include(":app", ":server", ":api", "cams-connection")
+
+val pIsBuildAgent: String by settings
+
+gradleEnterprise {
+    buildScan {
+        publishAlwaysIf(pIsBuildAgent.toBoolean())
+        termsOfServiceUrl = "https://gradle.com/terms-of-service"
+        termsOfServiceAgree = "yes"
+        isUploadInBackground = false
+    }
+}
