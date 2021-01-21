00f513f15659f32a1d30b63195e02e561bd515db
Vladislav Sumin <vladislavsumin@mail.ru>
2021-01-21 20:25:49 +0300

    [all] add check dependency update plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,6 @@
+import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
+import com.github.benmanes.gradle.versions.reporter.result.Result
+
 buildscript {
     repositories {
         mavenCentral()
@@ -12,6 +15,10 @@ buildscript {
     }
 }
 
+plugins {
+    id("com.github.ben-manes.versions") version "0.36.0"
+}
+
 allprojects {
     repositories {
         mavenCentral()
@@ -22,4 +29,27 @@ allprojects {
 
 tasks.create("clean", Delete::class) {
     delete(rootProject.buildDir)
+}
+
+fun isNonStable(version: String): Boolean {
+    val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.toUpperCase().contains(it) }
+    val regex = "^[0-9,.v-]+(-r)?$".toRegex()
+    val isStable = stableKeyword || regex.matches(version)
+    return isStable.not()
+}
+
+val pIsBuildAgent: String by project
+
+tasks.named("dependencyUpdates", DependencyUpdatesTask::class.java) {
+    rejectVersionIf {
+        isNonStable(candidate.version) && !isNonStable(currentVersion)
+    }
+
+    if (pIsBuildAgent.toBoolean()) {
+        this.outputFormatter = closureOf<Result> {
+            outdated.dependencies.forEach {
+                println("::warning::Library outdated: ${it.group}:${it.name} [${it.version} -> ${it.available.milestone}]}")
+            }
+        }
+    }
 }
