69e6c0c355505a50b5507279c682221f7a219a09
Vladislav Sumin <vladislavsumin@mail.ru>
2021-01-29 18:17:12 +0300

    [server] setup coroutine debug pretty print

--- server/src/main/kotlin/ru/falseteam/control/server/domain/debug/DebugInteractorImpl.kt
+++ server/src/main/kotlin/ru/falseteam/control/server/domain/debug/DebugInteractorImpl.kt
@@ -1,7 +1,10 @@
 package ru.falseteam.control.server.domain.debug
 
+import kotlinx.coroutines.CoroutineName
 import kotlinx.coroutines.debug.DebugProbes
+import kotlinx.coroutines.debug.State
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.withContext
 import org.slf4j.LoggerFactory
 import ru.falseteam.control.server.domain.configuration.ConfigurationInteractor
 
@@ -19,13 +22,20 @@ class DebugInteractorImpl(
         }
     }
 
-    override suspend fun run() {
+    override suspend fun run() = withContext(CoroutineName("debugger_dumper")) {
         while (true) {
             delay(10_000)
             val coroutines = DebugProbes.dumpCoroutinesInfo()
-            coroutines.forEach {
-                println(it)
-            }
+            val suspended = coroutines.count { it.state == State.SUSPENDED }
+            val running = coroutines.count { it.state == State.RUNNING }
+            log.debug(
+                "Dumped coroutines total=${coroutines.count()}, suspended=$suspended, running=$running"
+            )
+            val dump = coroutines.joinToString(
+                separator =
+                "\n\t", prefix = "Coroutines dump: \n\t"
+            )
+            log.trace(dump)
         }
     }
 }
