008daae06f1e9f9237f165fd23117b6eb49e4ffb
Vladislav Sumin <vladislavsumin@mail.ru>
2021-02-11 17:01:31 +0300

    [client] add entities primitive ui

--- app/src/main/java/ru/falseteam/control/ui/screens/entities/EntitiesScreen.kt
+++ app/src/main/java/ru/falseteam/control/ui/screens/entities/EntitiesScreen.kt
@@ -1,8 +1,48 @@
 package ru.falseteam.control.ui.screens.entities
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Card
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.unit.dp
 import ru.falseteam.control.di.kodeinViewModel
+import ru.falseteam.uikit.elements.UikitFullScreenProgressBar
 
 @Composable
 fun EntitiesScreen(viewModel: EntitiesViewModel = kodeinViewModel()) {
+    when (val state = viewModel.state.collectAsState().value) {
+        EntitiesState.Loading -> UikitFullScreenProgressBar()
+        is EntitiesState.ShowResult -> ShowResultScreen(state)
+    }
+}
+
+@Composable
+private fun ShowResultScreen(state: EntitiesState.ShowResult) {
+    LazyColumn {
+        items(state.entities) {
+            EntityCard(entity = it)
+        }
+    }
+}
+
+@Composable
+private fun EntityCard(entity: EntityUiModel) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(0.dp, 4.dp),
+        shape = RectangleShape
+    ) {
+        Column(modifier = Modifier.padding(16.dp, 8.dp)) {
+            Text(text = entity.id)
+            Text(text = entity.type)
+        }
+    }
 }
--- app/src/main/java/ru/falseteam/control/ui/screens/entities/EntitiesViewModel.kt
+++ app/src/main/java/ru/falseteam/control/ui/screens/entities/EntitiesViewModel.kt
@@ -1,9 +1,26 @@
 package ru.falseteam.control.ui.screens.entities
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.launch
 import ru.falseteam.control.domain.entities.EntitiesInteractor
 
 class EntitiesViewModel(
     private val entitiesInteractor: EntitiesInteractor,
 ) : ViewModel() {
+    val state = MutableStateFlow<EntitiesState>(EntitiesState.Loading)
+
+    init {
+        viewModelScope.launch {
+            entitiesInteractor.observeAll()
+                .map { entities ->
+                    entities.map { (id, entityInfo) -> EntityUiModel(id, entityInfo.type) }
+                }
+                .map { EntitiesState.ShowResult(it) }
+                .collect { state.value = it }
+        }
+    }
 }
