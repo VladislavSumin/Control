06cec8a4e7e9392e1ee57abbf943034270a43ae3
Vladislav Sumin <vladislavsumin@mail.ru>
2021-02-14 16:45:23 +0300

    [all] update kotlin from 1.4.21-2 to 1.4.30, update compose

--- app/src/main/java/ru/falseteam/control/ui/screens/addserver/AddServerScreen.kt
+++ app/src/main/java/ru/falseteam/control/ui/screens/addserver/AddServerScreen.kt
@@ -5,6 +5,8 @@ import androidx.compose.material.Text
 import androidx.compose.material.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.savedinstancestate.savedInstanceState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -29,7 +31,7 @@ fun AddServerScreen(viewModel: AddServerViewModel = kodeinViewModel()) {
 
 @Composable
 private fun InputState(viewModel: AddServerViewModel) {
-    val (url, setUrl) = savedInstanceState { "" }
+    val (url, setUrl) = rememberSaveable { mutableStateOf("") }
     Column(
         Modifier
             .fillMaxWidth()
