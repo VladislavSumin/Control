469e4cc65f6fa4ba73b67ed721976e6927af3865
Vladislav Sumin <vladislavsumin@mail.ru>
2021-02-27 23:18:05 +0300

    [all] update ktor from 1.5.1 to 1.5.2

--- buildSrc/src/main/java/ru/falseteam/config/Configuration.kt
+++ buildSrc/src/main/java/ru/falseteam/config/Configuration.kt
@@ -3,7 +3,7 @@ package ru.falseteam.config
 object Configuration {
     object Versions {
         const val kotlin = "1.4.30"
-        const val ktor = "1.5.1"
+        const val ktor = "1.5.2"
         const val log4j2 = "2.14.0"
         const val kodein = "7.3.1"
         const val sqldelight = "1.4.4"
